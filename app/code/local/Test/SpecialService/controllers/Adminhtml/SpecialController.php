285f677f3bd0668a7f8b8ba458b09bdbbd26f14f
Abdo Shajadi <abd.shajadi@gmail.com>
2016-11-25 00:11:37 +0200

    special block first

--- app/code/local/Test/SpecialService/controllers/Adminhtml/SpecialController.php
+++ app/code/local/Test/SpecialService/controllers/Adminhtml/SpecialController.php
@@ -4,11 +4,26 @@ class Test_SpecialService_Adminhtml_SpecialController extends Mage_Adminhtml_Con
 {
     public function indexAction()
     {
-        $this->loadLayout()
-            ->_setActiveMenu('specialservicetab')
-            ->_title($this->__('UK'));
+//        $this->loadLayout()
+//            ->_setActiveMenu('specialservicetab')
+//            ->_title($this->__('UK'));
+//
+//        // my stuff
+//
+//        $this->renderLayout();
 
-        // my stuff
+
+        $this->loadLayout();
+
+        $block = $this->getLayout()->createBlock(
+            'Mage_Core_Block_Template',
+            'my_block_name_here',
+            array('template' => 'testspecial/test_specialservice_core_block.phtml')
+        );
+
+        //$this->_addContent($block);
+
+        $this->_addLeft($this->getLayout()->createBlock('Test_SpecialServiceBlock_ShowTabsAdminBlock'));
 
         $this->renderLayout();
     }
